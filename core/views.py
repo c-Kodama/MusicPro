e3ba560d5519e8210554004d314dab2b94a158b0
Constanza Cifuentes <con.cifuentesc@duocuc.cl>
2022-05-27 14:54:39 -0400

    consumo API producto v1

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+import requests
 
-def home(request):
-    return render(request, 'core/home.html')
+r = requests.get('http://127.0.0.1:8000/api/categorias/')
+
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+    
+def categorias(request):
+    response = requests.get('http://127.0.0.1:8000/api/categorias/')
+    categorias = response.json()
+    print(categorias)
+
+    return render(request, "core/categorias.html", {'categorias':categorias})
+    pass 
+
+
